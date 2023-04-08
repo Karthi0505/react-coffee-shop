fbd977acfe55cafefcae26982e3daa308d347c68
Karthi0505 <karthi.uideveloper@gmail.com>
2023-04-09 02:08:09 +0530

    animation commit

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import formatCurrency from "../util";
+import Fade from 'react-reveal/Fade';
 
 export default class Cart extends Component {
   constructor(props) {
@@ -41,6 +42,7 @@ export default class Cart extends Component {
 
         <div>
           <div className="cart">
+            <Fade left cascade>
             <ul className="cart-items">
               {cartItems.map((item) => (
                 <li key={item._id}>
@@ -60,7 +62,8 @@ export default class Cart extends Component {
                   </div>
                 </li>
               ))}
-            </ul>
+              </ul>
+              </Fade>
           </div>
 
           {cartItems.length !== 0 && (
