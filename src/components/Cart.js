319ddbb98fe6fb7d8bdd54c89430eccec11148f8
Karthi0505 <karthi.uideveloper@gmail.com>
2023-04-10 19:44:26 +0530

    commit redux cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
 import formatCurrency from "../util";
 import Fade from 'react-reveal/Fade';
+import { connect } from "react-redux";
+import { removeFromCart } from "../actions/cartActions";
 
-export default class Cart extends Component {
+class Cart extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -55,7 +57,8 @@ export default class Cart extends Component {
                       {formatCurrency(item.price)} x {item.count}{" "}
                       <button
                         className="button"
-                        onClick={() => this.props.removeFromCart(item)}>
+                        onClick={() => this.props.removeFromCart(item)}
+                      >
                         Remove
                       </button>
                     </div>
@@ -118,3 +121,9 @@ export default class Cart extends Component {
     );
   }
 }
+export default connect(
+  (state) => ({
+    cartItems: state.cart.cartItems,
+  }),
+  {removeFromCart}
+)(Cart);
