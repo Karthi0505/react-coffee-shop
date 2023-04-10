e4831cc0fafecd2c0b37cf2fd75bf1fc789c5877
Karthi0505 <karthi.uideveloper@gmail.com>
2023-04-10 23:35:22 +0530

    before removing dollar format - has error

--- server.js
+++ server.js
@@ -49,5 +49,58 @@ app.delete(
     }
 );
 
+//first parameter in mongoose.model is collection name
+const Order = mongoose.model(
+  "orders",
+  new mongoose.Schema(
+    {
+      _id: {
+        type: String,
+        default: shortid.generate
+      },
+      email: String,
+      name: String,
+      address: String,
+      total: Number,
+      cartItems: [
+        {
+          _id: String,
+          title: String,
+          price: Number,
+          count: Number
+        },
+      ],
+    },
+    {
+      timestamps: true,
+    }
+  )
+);
+
+app.post("/api/orders", async (req, res) => {
+  if (!req.body.name ||
+    !req.body.email ||
+    !req.body.address ||
+    !req.body.total ||
+    !req.body.cartItems
+  ) {
+    return res.send({ message: "Data is required." });
+  }
+
+  const order = await Order(req.body).save();
+  res.send(order);
+
+});
+
+app.get("/api/orders", async (req, res) => {
+  const orders = await Order.find({});
+  res.send(orders);
+});
+
+app.delete("/api/orders/:id", async (req, res) => {
+  const order = await Order.findByIdAndDelete(req.params.id);
+  res.send(order);
+});
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log("Serve at http://localhost:5000"));
